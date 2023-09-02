176d7d16220629617482183658b1a7df1272f71b
Anthony Hernandez <anthonyrhernandez6@gmail.com>
2023-09-02 12:38:45 -0600

    Commmit

--- src/main/java/Pizza/Base/Especialidades/Italiana.java
+++ src/main/java/Pizza/Base/Especialidades/Italiana.java
@@ -1,2 +1,26 @@
-package Pizza.Base.Especialidades;public class Italiana {
+package Pizza.Base.Especialidades;
+
+import Pizza.Base.Pizza;
+import Pizza.Base.Topping;
+public class Italiana extends Pizza{
+
+    private String salsa;
+
+    public  Italiana(String name, double price, String salsa, Topping... toppings){
+        super(name,
+
+                toppings);
+        this.salsa=salsa;
+
+    }
+
+
+    public String getSalsa() {
+        return salsa;
+    }
+
+    public void setSalsa(String salsa) {
+        this.salsa = salsa;
+    }
+
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,34 @@
 package org.example;
-
+import Pizza.Base.Especialidades.Italiana;
+import Pizza.Base.Pizza;
+import Pizza.Base.Topping;
+import Pizza.Base.Especialidades.Italiana;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Pizza pizza = new Pizza("Pizza Margarita");
+        pizza.addTopping(new Topping("Tomato",10));
+        pizza.addTopping(new Topping("Mozzarella",20));
+        pizza.addTopping(new Topping("Brocoli",10));
+        pizza.prepare();
+        System.out.println("PRecio Toltal: "+pizza.getPizzaPrice());
+
+
+/*
+        //Quitar mozzarela
+        pizza.removeTopping(1);
+
+        //Agregar Mozzarella de nuevo
+        pizza.addTopping(new Topping("Mozzarella",20));
+
+        pizza.prepare();
+
+        Italiana italiana = new Italiana("Italiana", 20, "Especial");
+        italiana.addTopping(new Topping("Tomato",20));
+        italiana.prepare();*/
+
+
+
+
     }
 }
